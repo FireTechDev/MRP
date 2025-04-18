0f1141847f230d91495a520b8eca05a15de628c1
FireTechDev <FireTechDev@users.noreply.github.com>
2025-04-18 10:31:09 +0200

    Move icons to root directory

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@ const urlsToCache = [
   '.',
   'index.html',
   'manifest.json',
-  'icons/icon-192x192.png',
-  'icons/icon-512x512.png'
+  'icon-192x192.png',
+  'icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
