c04d37990393d20dbe2fe9e10c6ac98d723e08e4
FireTechDev <FireTechDev@users.noreply.github.com>
2026-03-06 15:28:20 +0100

    Ship inter spacing fix with new build

--- script.js
+++ script.js
@@ -1947,7 +1947,7 @@ function toggleMoyensDepart() {
 
 // Version de l'application
 const APP_VERSION = '1.0.22';
-const APP_BUILD = '06/03/2026 - 14h29';
+const APP_BUILD = '06/03/2026 - 15h27';
 const PWA_VERSION_ENDPOINT = './version.json';
 const PWA_SW_URL = `./sw.js?v=${encodeURIComponent(`${APP_VERSION}-${APP_BUILD}`)}`;
 const PWA_VERSION_CHECK_INTERVAL_MS = 10 * 60 * 1000;
